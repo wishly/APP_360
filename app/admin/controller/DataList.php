1d21b00effdf74d86fad3a6fd9a6f38e68f74ccb
wishly <517642227@qq.com>
2017-12-10 15:44:39 +0800

    完善部分后台添加数据功能页面

--- app/admin/controller/DataList.php
+++ app/admin/controller/DataList.php
@@ -8,6 +8,7 @@
 
 namespace app\admin\controller;
 
+use think\Request;
 use think\Session;
 use app\admin\common\Base as BaseController;
 //use think\paginator;
@@ -24,7 +25,7 @@ class DataList extends BaseController {
             $map['title'] = array('like',"%$keyword%"); //数组模糊查询
             $this->assign('keyword', $keyword);
         }
-        $row = 5;
+        $row = 7;
         $count = model('Datalist')->where($map)->count();
         $datalist = model('Datalist')->where($map)->paginate($row, $count);
         $page = $datalist->render();
@@ -33,5 +34,48 @@ class DataList extends BaseController {
         $this->assign('datalist', $datalist);
 
         return $this->fetch('DataList/lists');
+    }
+
+    public function add(){
+        $this->assign('username', Session::get('user_name')); //显示登录用户名
+        $title = '添加数据';    //添加数据
+        $this->assign('title', $title);
+        $datalist = model("Datalist");
+        if(Request::instance()->isPost()){
+
+        } else {
+            $map["id"] = input("id", '', "int");
+            $datalist = $datalist->where($map)->find();
+            $high_level = model("high_level")->select();
+            $this->assign("high_level", $high_level);  //高级分类数据
+            $this->assign("data", $datalist);          //分类数据
+            return $this->fetch('DataList/add');
+        }
+    }
+    public function getMiddleInfo(){
+        $map["high_id"] = input('high_id',"", "int");
+        $middle = model("MiddleLevel")->where($map)->field("id,middle_name")->select();
+        $option = '';
+        foreach( $middle as $vo ) {
+            $option .= "<option value=".$vo['id'].">".$vo['middle_name']."</option>";
+        }
+        return $option;
+    }
+    public function getElementaryInfo(){
+        $map["middle_id"] = input("middle_id", "", "int");
+        $element = model("ElementaryLevel")->where($map)->field("id,elementary_name")->select();
+        $option = '';
+        foreach( $element as $vo ) {
+            $option .= "<option value=".$vo['id'].">".$vo['elementary_name']."</option>";
+        }
+        return $option;
+    }
+
+    public function edit(){
+
+    }
+
+    public function delete() {
+
     }
 }
